8f446c20525649b0a7ebbb51973b10ad7cf1973a
WWyane <wesleywyane@gmail.com>
2019-07-05 14:02:02 -0700

    linked user to posts and profile, handled image upload on backend

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <div class="row">
+    <div class="col-3 p-5">
+        <img src="http://sextuple-u.biz/static/media/floppyWlogo.488dc469.png" style="width: 100%">
+    </div>
+    <div class="col-9">
+        <div class="d-flex justify-content-between align-items-baseline">
+            <h1>{{ $user->username }}</h1>
+            <a href="/p/create">Add New Post</a>
+        </div>
+        <div class="d-flex">
+            <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
+            <div class="pr-5"><strong>34k</strong> followers</div>
+            <div class="pr-5"><strong>98</strong> following</div>
+        </div>
+        <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+        <div>{{ $user->profile->description }}</div>
+        <div><a href="http://sextuple-u.biz">{{ $user->profile->url }}</a></div>
+    </div>
+   </div>
+   
+   <div class="row pt-5">
+        @foreach($user->posts as $post)
+            <div class="col-4 pb-4">
+                <img src="/storage/{{ $post->image }}" class="w-100">
+            </div>
+        @endforeach
+    
+    
+   </div>
+</div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/profile/{$user->user_id}') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
