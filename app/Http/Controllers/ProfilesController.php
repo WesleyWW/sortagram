9d807189f3259f5f8da1aaac6011caaa130d33af
WWyane <wesleywyane@gmail.com>
2019-07-09 19:45:18 -0700

    changed index page to profile index which displays posts from profiles that are followed

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 
@@ -12,8 +13,27 @@ class ProfilesController extends Controller
     public function index(User $user)
     {
         $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
+
+        $postCount = Cache::remember(
+            'count.posts.' . $user->id, 
+            now()->addSeconds(30), 
+            function () use ($user) {
+                return $user->posts->count();
+        });  
+        $followerCount = Cache::remember(
+            'count.followers.' . $user->id, 
+            now()->addSeconds(30), 
+            function () use ($user) {
+                return $user->profile->followers->count();
+        });
+        $followingCount = Cache::remember(
+            'count.following.' . $user->id, 
+            now()->addSeconds(30), 
+            function () use ($user) {
+                return $user->following->count();
+        });
         
-        return view('profiles.index', compact('user', 'follows'));
+        return view('profiles.index', compact('user', 'follows', 'postCount', 'followerCount', 'followingCount'));
     }
 
     public function edit(User $user)
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    @foreach($posts as $post)
+        <div class="row">
+            <div class="col-6 offset-3">
+                <img src="/storage/{{ $post->image }}" class="w-100">
+            </div>
+        </div>
+        <div class="row pt-2 pb-4">
+            <div class="col-6 offset-3">
+                <p><span class="font-weight-bold" style="padding-right: 6px"><a href="/profile/{{ $post->user->id }}"><span class="text-dark">{{ $post->user->username }}</span></a></span>{{ $post->caption  }}</p>
+            </div>
+        </div>
+    @endforeach    
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+            {{ $posts->links() }}
+        </div>
+    </div>
+</div>
+@endsection
